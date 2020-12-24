05abb7124f9dd36f5895cd1ad1bc480df99735aa
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2020-12-24 21:20:09 +0200

    Добавляет новые данные

--- src/js/inquire.js
+++ src/js/inquire.js
@@ -1,34 +1,58 @@
 import templateFirst from "../template/first.hbs";
 import templateSecond from "../template/second.hbs";
 
+import debounce from "lodash.debounce";
+
+//console.log(debounce);
+
+//todo: Доступ к инпуту
+const input = document.querySelector(".control");
+//console.log(input);
+
+//todo: Доступ к ul
 const access = document.querySelector(".country");
-console.log(access);
+//console.log(access);
 
 let search = `Switzerland`;
 let url = `https://restcountries.eu/rest/v2/name/${search}`;
 
-// const myRequest = new XMLHttpRequest();
-// console.log(myRequest);
+input.addEventListener("input", debounce(onSearch, 2000));
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const searchQuery = e.currentTarget.value;
+  console.log(searchQuery);
+}
+
+function debounce(callback, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, delay);
+  };
+}
+
 let myFirstFetch = fetch(url);
-console.log(myFirstFetch);
+//console.log(myFirstFetch);
 
 myFirstFetch
   .then((response) => {
-    console.log(response);
+    //console.log(response);
 
     return response.json();
   })
   .then((result) => {
-    console.log(result);
+    //console.log(result);
 
     const array = result.capital;
-    console.log(array);
+    //console.log(array);
 
     const item = templateFirst(result);
-    console.log(item);
+    //console.log(item);
 
     const secondItem = templateSecond(result);
-    console.log(secondItem);
+    //console.log(secondItem);
 
     access.insertAdjacentHTML("afterbegin", item);
 
